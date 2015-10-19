e068aea3ba0aea106b7e8639606fb1d34b5d3278
Tatchagon <o_k_t@hotmail.com>
2015-10-19 17:12:20 +0700

    7.3.1 A working form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,13 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
+
+  def create
+  	@user = User.new(params[:user]) # Not the final implementation!
+  	if @user.save
+  		# Handle a successful save.
+  	else
+  		render 'new'
+  	end
+  end
 end
