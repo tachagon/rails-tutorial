e5fc62d131280340c2eafd0043dd7e2f9bfe4f2e
Tatchagon <o_k_t@hotmail.com>
2015-10-21 16:59:33 +0700

    8.2.2 Current user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,9 @@ module SessionsHelper
 	def log_in(user)
 		session[:user_id] = user.id
 	end
+
+	# Return the current logged-in user (if any).
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
 end
