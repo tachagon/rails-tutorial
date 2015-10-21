90cfbc74248e1bdc0d4daecd4db85b5fea40d63c
Tatchagon <o_k_t@hotmail.com>
2015-10-21 18:02:18 +0700

    8.4.1 Remember token and digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    attr_accessor :remember_token
     before_save {email.downcase!}
     validates :name, presence: true, length: {maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -14,4 +15,15 @@ class User < ActiveRecord::Base
                                                   BCrypt::Engine.cost
     	BCrypt::Password.create(string, cost: cost)
   	end
+
+    # Returns a random token.
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    # Remembers a user in the database for user in persistent sessions.
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
 end
