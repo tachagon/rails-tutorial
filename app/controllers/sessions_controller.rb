3fe065cacb474d7be6b986ab8ba2b25d28d03c60
Tatchagon <o_k_t@hotmail.com>
2015-10-21 15:18:44 +0700

    8.1.4 Rendering with a flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   	if user && user.authenticate(params[:session][:password])
   		# Log the user in and redirect to the user's show page.
   	else
-  		# Create an error message.
+  		flash[:danger] = 'Invalid email/password combination' # Not quite right!
   		render 'new'
   	end
   end
