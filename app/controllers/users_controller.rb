16703b17799f01dfbdf326ccb668d6f0aa203534
Tatchagon <o_k_t@hotmail.com>
2015-12-05 23:04:54 +0700

    9.1.2 Unsuccessful edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(user_params) # Not the final implementation!
+  	@user = User.new(user_params)
   	if @user.save
   		log_in @user
   		flash[:success] = "Welcome to the Sample App!"
@@ -21,6 +21,15 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
   end
 
   private
