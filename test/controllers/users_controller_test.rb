69f506842915a34cf03bca25b9e8c35c156afe9f
Tatchagon <o_k_t@hotmail.com>
2015-12-10 20:51:14 +0700

    Finish Chapter 9

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -43,6 +43,17 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   end
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch :update, id: @other_user, user:{
+      password: '123456',
+      password_confirmation: '123456',
+      admin: true
+    }
+    assert_not @other_user.admin?
+  end
+
   test "should redirect destroy when not logged in" do
     assert_no_difference 'User.count' do
       delete :destroy, id: @user
