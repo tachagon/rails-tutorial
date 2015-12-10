7700db4a8b427f2fa9259589f6825f5d195dddfd
Tatchagon <o_k_t@hotmail.com>
2015-12-10 12:27:42 +0700

    9.2.1 Requiring logged-in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:edit, :update]
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -34,8 +36,19 @@ class UsersController < ApplicationController
   end
 
   private
+
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
+
+    # before filters
+    # 
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 
 end
