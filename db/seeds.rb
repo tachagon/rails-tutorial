27cd3db7bb800d9e8f97683846680e0ca0126256
Tatchagon <o_k_t@hotmail.com>
2015-12-15 22:27:22 +0700

    12.2.1 Sample following data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
+# Users
 User.create!(
 		name: "Examples User",
 		email: "example@railstutorial.org",
@@ -26,9 +19,17 @@ User.create!(
 		)
 end
 
-
+# Microposts
 users = User.order(:created_at).take(6)
 50.times do
 	content = Faker::Lorem.sentence(5)
 	users.each{|user| user.microposts.create!(content: content)}
 end
+
+# Following relationship
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each{ |followed| user.follow(followed) }
+followers.each{ |follower| follower.follow(user) }
