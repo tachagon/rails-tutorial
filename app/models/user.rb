c8071d1684670f7a5559ba95aeb9f90724e1dbf4
Tatchagon <o_k_t@hotmail.com>
2015-12-16 00:17:37 +0700

    12.3.2 A first feed implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,7 +46,7 @@ class User < ActiveRecord::Base
     # Defines a proto-feed.
     # See "Following users" for the full implementation.
     def feed
-        Micropost.where("user_id = ?", id)
+        Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
     end
 
     # Follows a user.
