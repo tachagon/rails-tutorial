5608cf7a073989cb752ac46bdd346e4e31f0a463
Tatchagon <o_k_t@hotmail.com>
2015-12-15 17:26:54 +0700

    11.3.5

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -20,4 +20,13 @@ class MicropostsControllerTest < ActionController::TestCase
 		assert_redirected_to login_url
 	end
 
+	test "should redirect destroy for wrong micropost" do
+		log_in_as(users(:michael))
+		micropost = microposts(:ants)
+		assert_no_difference "Micropost.count" do
+		  delete :destroy, id: micropost
+		end
+		assert_redirected_to root_url
+	end
+
 end
