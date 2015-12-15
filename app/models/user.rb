0391f28d95dba32c2b6687ebcfa1661501f9cc00
Tatchagon <o_k_t@hotmail.com>
2015-12-16 00:26:25 +0700

    12.3.3 Subselects

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,7 +46,8 @@ class User < ActiveRecord::Base
     # Defines a proto-feed.
     # See "Following users" for the full implementation.
     def feed
-        Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+        following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+        Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
     end
 
     # Follows a user.
