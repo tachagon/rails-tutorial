217618b83f5886377404e383e5abc3f470e38529
Tatchagon <o_k_t@hotmail.com>
2015-12-15 12:29:35 +0700

    11.1.4 Micropost refinements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-    has_many :microposts
+    has_many :microposts, dependent: :destroy
     attr_accessor :remember_token
     before_save {email.downcase!}
     validates :name, presence: true, length: {maximum: 50}
