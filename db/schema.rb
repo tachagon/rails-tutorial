cc14f0ed91e2df3d7896ab6cb26535d51cd24af8
Tatchagon <o_k_t@hotmail.com>
2015-12-15 14:43:56 +0700

    11.2.2 Sample microposts

--- db/schema.rb
+++ db/schema.rb
@@ -26,14 +26,11 @@ ActiveRecord::Schema.define(version: 20151214091443) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin",             default: false
-    t.string   "activation_digest"
-    t.boolean  "activated",         default: false
-    t.datetime "activated_at"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
